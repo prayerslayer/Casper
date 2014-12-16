80ebb8e4aad178ae89df11f0edf88b9512a6904e
Nikolaus Piccolotto <nikolaus.piccolotto@zalando.de>
2014-12-16 11:43:48 +0100

    Local fonts

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,43 +8,50 @@ var gulp = require( 'gulp' ),
     vendor = require( 'gulp-autoprefixer' );
 
 gulp.task( 'build:less', function() {
-    gulp
-        .src( srcDir + 'assets/less/main.less' )
-        .pipe( less() )
-        .pipe( vendor() )
-        .pipe( gulp.dest( dstDir + 'assets/css/' ) );
+    return gulp
+            .src( srcDir + 'assets/less/main.less' )
+            .pipe( less() )
+            .pipe( vendor() )
+            .pipe( gulp.dest( dstDir + 'assets/css/' ) );
 });
 
 gulp.task( 'build:templates', function() {
-    gulp
-        .src( srcDir + 'templates/**/*.hbs' )
-        .pipe( gulp.dest( dstDir ) );
+    return gulp
+            .src( srcDir + 'templates/**/*.hbs' )
+            .pipe( gulp.dest( dstDir ) );
+});
+
+gulp.task( 'build:fonts', function() {
+    return gulp
+            .src( srcDir + 'assets/fonts/**/*' )
+            .pipe( gulp.dest( dstDir + 'assets/fonts/' ) );
 });
 
 gulp.task( 'build:css', [ 'build:less' ], function() {
-    gulp
-        .src([
-            srcDir + 'assets/css/normalize.css',
-            srcDir + 'assets/css/skeleton.css',
-            srcDir + 'assets/css/main.css' ])
-        // .pipe( concat( 'site.css' ) )
-        .pipe( gulp.dest( dstDir + 'assets/css/' ) );
+    return gulp
+            .src([
+                srcDir + 'assets/css/normalize.css',
+                srcDir + 'assets/css/skeleton.css',
+                srcDir + 'assets/css/main.css' ])
+            // .pipe( concat( 'site.css' ) )
+            .pipe( gulp.dest( dstDir + 'assets/css/' ) );
 });
 
 gulp.task( 'build', [
     'build:less',
     'build:css',
-    'build:templates'
+    'build:templates',
+    'build:fonts'
 ]);
 
 gulp.task( 'apply', [ 'build' ], function() {
-    gulp
-        .src( dstDir + '**/*' )
-        .pipe( gulp.dest( ghostDir ) );
+    return gulp
+            .src( dstDir + '**/*' )
+            .pipe( gulp.dest( ghostDir ) );
 });
 
 gulp.task( 'watch', function() {
-    gulp.watch( srcDir + '**/*.*', [ 'apply' ]);
+    return gulp.watch( srcDir + '**/*.*', [ 'apply' ]);
 });
 
 gulp.task( 'default', ['watch'] );
