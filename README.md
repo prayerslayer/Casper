5b33ce4068d6c67cf0055b77448b51a7ff160627
Nikolaus Piccolotto <nikolaus.piccolotto@zalando.de>
2014-12-18 22:53:59 +0100

    page footer

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # TODO
 
-* Post Page
-    * Footer
+
 * Related Posts
 * Page Header
     * Header Menu Links
 * Index Page
     * Featured?
 * Collection Page
-* Page Footer
 * Typography, Animations
 * Responsiveness
 * Offline Caching für billder, fonts
--- src/assets/js/attributes/fit-width-height.js
+++ src/assets/js/attributes/fit-width-height.js
@@ -3,12 +3,18 @@
 
     function updateFullWidthElements() {
         var windowWidth = $(window).width();
-        $( '[data-fit-window-width]' ).css( 'width', windowWidth );
+        $( '[data-fit-window-width]' ).each( function() {
+            var $this = $( this );
+            $this.css( 'width', parseInt( $this.attr('data-fit-window-width'), 10 ) + windowWidth );
+        });
     }
 
     function updateFullHeightElements() {
         var windowHeight = $(window).height();
-        $( '[data-fit-window-height]' ).css( 'height', windowHeight );
+        $( '[data-fit-window-height]' ).each( function() {
+            var $this = $( this );
+            $this.css( 'height', parseInt( $this.attr('data-fit-window-height'), 10 ) + windowHeight );
+        });
     }
 
     $(document).ready( updateFullWidthElements );
